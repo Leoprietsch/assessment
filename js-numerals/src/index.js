e7f7738f7604eb7cb21c82dace41f6d0834d7ae6
Leoprietschm <leo.prietsch@gmail.com>
2021-02-28 17:49:01 -0300

    feat: add larger numbers to name slices

--- js-numerals/src/index.js
+++ js-numerals/src/index.js
@@ -32,6 +32,50 @@ const TENS = {
   9: "ninety",
 };
 
+const LARGES = {
+  0: "thousand",
+  1: "million",
+  2: "billion",
+  3: "trillion",
+  4: "quadrillion",
+  5: "quintillion",
+  6: "sextillion",
+  7: "septillion",
+  8: "octillion",
+  9: "nonillion",
+  10: "decillion",
+  11: "undecillion",
+  12: "duodecillion",
+  13: "tredecillion",
+  14: "quattuordecillion",
+  15: "quindecillion",
+  16: "sedecillion",
+  17: "septendecillion",
+  18: "octodecillion",
+  19: "novendecillion",
+  20: "vigintillion",
+  21: "unvigintillion",
+  22: "duovigintillion",
+  23: "tresvigintillion",
+  24: "quattuorvigintillion",
+  25: "quinvigintillion",
+  26: "sesvigintillion",
+  27: "septemvigintillion",
+  28: "octovigintillion",
+  29: "novemvigintillion",
+  30: "trigintillion",
+  31: "untrigintillion",
+  32: "duotrigintillion",
+  33: "trestrigintillion",
+  34: "quattuortrigintillion",
+  35: "quintrigintillion",
+  36: "sestrigintillion",
+  37: "septentrigintillion",
+  38: "octotrigintillion",
+  39: "noventrigintillion",
+  40: "quadragintillion",
+};
+
 const HUNDRED = "hundred";
 
 const SPACE = " ";
@@ -45,8 +89,9 @@ function convertNumberToWords() {
   } else {
     let slicedNumbers = sliceNumber(number, 3).reverse();
 
-    slicedNumbers.forEach((numberSlice) => {
+    slicedNumbers.forEach((numberSlice, index) => {
       let number = Number(numberSlice);
+      if (number === 0) return;
       let slicedSlice = sliceNumber(number, 1);
       const digits = slicedSlice.length;
 
@@ -81,7 +126,10 @@ function convertNumberToWords() {
         }
       }
 
-      !sliceWords || numberWords.unshift(sliceWords.join(SPACE));
+      if (sliceWords) {
+        if (index > 0) sliceWords.push(LARGES[index - 1]);
+        numberWords.unshift(sliceWords.join(SPACE));
+      }
     });
   }
   alert(numberWords.join(SPACE));
