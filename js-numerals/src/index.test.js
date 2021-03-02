e7432cc561fb635ffb0b883c6d40216aa982277f
Leoprietschm <leo.prietsch@gmail.com>
2021-03-02 01:16:19 -0300

    tests: zero to nineteen

--- js-numerals/src/index.test.js
+++ js-numerals/src/index.test.js
@@ -18,4 +18,32 @@ function test(testFunction, parameter, expectedOutput) {
   console.assert(result === expectedOutput);
 }
 
-test(convertNumberToWords, 10, "ten");
+function runConvertNumberToWordsTests() {
+  console.log("Convert number function tests");
+
+  //setup ones
+  test(convertNumberToWords, 0, "zero");
+  test(convertNumberToWords, 1, "one");
+  test(convertNumberToWords, 2, "two");
+  test(convertNumberToWords, 3, "three");
+  test(convertNumberToWords, 4, "four");
+  test(convertNumberToWords, 5, "five");
+  test(convertNumberToWords, 6, "six");
+  test(convertNumberToWords, 7, "seven");
+  test(convertNumberToWords, 8, "eight");
+  test(convertNumberToWords, 9, "nine");
+
+  //setup teens
+  test(convertNumberToWords, "10", "ten");
+  test(convertNumberToWords, "11", "eleven");
+  test(convertNumberToWords, "12", "twelve");
+  test(convertNumberToWords, "13", "thirteen");
+  test(convertNumberToWords, "14", "fourteen");
+  test(convertNumberToWords, "15", "fifteen");
+  test(convertNumberToWords, "16", "sixteen");
+  test(convertNumberToWords, "17", "seventeen");
+  test(convertNumberToWords, "18", "eighteen");
+  test(convertNumberToWords, "19", "nineteen");
+}
+
+runConvertNumberToWordsTests();
