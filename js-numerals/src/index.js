eb15682873a85456ccaf6cf8b43084c72db8f946
Leoprietschm <leo.prietsch@gmail.com>
2021-03-02 00:43:11 -0300

    refactor: number words constants and dom manipulation

--- js-numerals/src/index.js
+++ js-numerals/src/index.js
@@ -1,80 +1,14 @@
-const NUMBERS = {
-  ONES: {
-    0: "zero",
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five",
-    6: "six",
-    7: "seven",
-    8: "eight",
-    9: "nine",
-    10: "ten",
-    11: "eleven",
-    12: "twelve",
-    13: "thirteen",
-    14: "fourteen",
-    15: "fifteen",
-    16: "sixteen",
-    17: "seventeen",
-    18: "eighteen",
-    19: "nineteen",
-  },
-  TENS: {
-    2: "twenty",
-    3: "thirty",
-    4: "forty",
-    5: "fifty",
-    6: "sixty",
-    7: "seventy",
-    8: "eighty",
-    9: "ninety",
-  },
-  HUNDRED: "hundred",
-};
+import { NUMBERS, LARGE_NUMBERS } from "./numbers.js";
 
-const LARGE_NUMBERS = {
-  ONES: {
-    0: "thousand",
-    1: "million",
-    2: "billion",
-    3: "trillion",
-    4: "quadrillion",
-    5: "quintillion",
-    6: "sextillion",
-    7: "septillion",
-    8: "octillion",
-    9: "nonillion",
-  },
-  TENS: {
-    1: "decillion",
-    2: "vigintillion",
-    3: "trigintillion",
-    4: "quadragintillion",
-    5: "quinquagintillion",
-    6: "sexagintillion",
-    7: "septuagintillion",
-    8: "octogintillion",
-    9: "nonagintillion",
-  },
-  UNITS_PREFIX: {
-    1: "un",
-    2: "duo",
-    3: "tres",
-    4: "quattuor",
-    5: "quin",
-    6: "sex",
-    7: "septen",
-    8: "octo",
-    9: "noven",
-  },
-};
+document.querySelector("form").submit(function (event) {
+  event.preventDefault();
+});
 
-function convertNumberToWords() {
-  let number = BigInt(document.getElementById("number").value);
-  if (number == 0) return alert(NUMBERS.ONES[number]);
+document.querySelector("button").addEventListener("click", () => (document.getElementById("").innerHTML = convertNumberToWords(document.getElementById("number").value)));
 
+function convertNumberToWords(number) {
+  number = BigInt(number);
+  if (number == 0) return NUMBERS.ONES[number];
   let words = [];
   const SPACE = " ";
 
