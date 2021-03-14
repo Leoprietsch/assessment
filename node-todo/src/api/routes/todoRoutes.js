32af4bdd10bebf242c097789e9e55c5301dfd2c6
Leoprietsch <leo.prietsch@gmail.com>
2021-03-14 00:46:53 -0300

    feat: add required routes

--- node-todo/src/api/routes/todoRoutes.js
+++ node-todo/src/api/routes/todoRoutes.js
@@ -1,7 +1,23 @@
 var todoRoutes = require("express").Router();
 
 todoRoutes.get("/", (request, response) => {
-  response.send("Hello World!");
+  response.send("GET TODOS");
+});
+
+todoRoutes.post("/", (request, response) => {
+  response.send("POST TODO");
+});
+
+todoRoutes.get("/:id", (request, response) => {
+  response.send("GET TODO");
+});
+
+todoRoutes.put("/:id", (request, response) => {
+  response.send("PUT TODO");
+});
+
+todoRoutes.delete("/:id", (request, response) => {
+  response.send("DELETE TODO");
 });
 
 module.exports = todoRoutes;
