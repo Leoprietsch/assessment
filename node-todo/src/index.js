9c95227b6214671f2d323f259f3e24af58f8358b
Leoprietsch <leo.prietsch@gmail.com>
2021-03-13 22:42:07 -0300

    build: app file for middleware and route configs

--- node-todo/src/index.js
+++ node-todo/src/index.js
@@ -1,10 +1,5 @@
-const express = require("express");
-const app = express();
 const port = 5000;
-
-app.get("/", (request, response) => {
-  response.send("Hello World!");
-});
+const app = require("./app");
 
 app.listen(port, () => {
   console.log(`To-Do API listening at http://localhost:${port}`);
