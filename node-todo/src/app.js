d9cc78a1fa1aba8434f87f6b8a337d470a727456
Leoprietsch <leo.prietsch@gmail.com>
2021-03-15 14:40:43 -0300

    feat: error handling middleware using http-errors

--- node-todo/src/app.js
+++ node-todo/src/app.js
@@ -6,4 +6,11 @@ app.use(express.json());
 
 app.use("/todos", todoRoutes);
 
+app.use((error, request, response, next) => {
+  console.log(error);
+
+  response.status(error.status);
+  response.json({ error: error.message });
+});
+
 module.exports = app;
